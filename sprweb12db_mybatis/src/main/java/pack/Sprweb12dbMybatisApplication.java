f49e4bffc7f8060664a31be2deb271a91328231a
hyoseonlim <hyoseonee0729@naver.com>
2024-07-17 17:36:16 +0900

    Spring Boot DB connection - MyBatis

---
+++ sprweb12db_mybatis/src/main/java/pack/Sprweb12dbMybatisApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb12dbMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb12dbMybatisApplication.class, args);
+	}
+
+}
